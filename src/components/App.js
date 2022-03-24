c43049a6369da29240a4f390a624c9a2086ec822
alef-garrido <oa.g11@icloud.com>
2022-03-23 19:08:38 -0600

    Initialize React Router

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,15 @@
+import { Routes, Route, Link } from "react-router-dom";
+
 const App = () => {
-  return(
-    <h1>
-      Werk!
-    </h1>
-  )
-}
+  return (
+    <div className="app--container">
+      <h1>Werk!</h1>;
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+      </Routes>
+    </div>
+  );
+};
 
 export default App;
