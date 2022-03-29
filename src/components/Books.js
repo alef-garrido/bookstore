eec9608705e2f65c5102200563923c2ef65f5378
alef-garrido <oa.g11@icloud.com>
2022-03-29 16:23:59 -0600

    Consume state inside Books component

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,12 +1,14 @@
-import Book from './Book';
+import { useSelector } from 'react-redux';
+// import Book from './Book';
 import BookForm from './BookForm';
 
 function Books() {
+  const books = useSelector((state) => state.books);
   return (
     <div className="book--list--container">
       <section className="book--list">
         <ul>
-          <Book />
+          {books}
         </ul>
       </section>
       <BookForm />
