18bc4d4e9e9655be18228474cd52053a74b2719e
alef-garrido <oa.g11@icloud.com>
2022-03-30 12:11:04 -0600

    Added Status message to Categories component

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,17 +1,21 @@
-import { useDispatch } from 'react-redux';
+import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { getStatus } from '../redux/categories/categories';
 
 function Categories() {
+  const [visible, setVisible] = useState(false);
+  const status = useSelector((status) => status.categoriesReducer);
   const dispatch = useDispatch();
+
   const clicklHandler = () => {
+    setVisible((prevState) => !prevState);
     dispatch(getStatus());
   };
+
   return (
     <section className="categories--container">
-      <button
-        type="button"
-        onClick={clicklHandler}
-      >
+      <div className={visible ? null : 'hide'}>{status}</div>
+      <button type="button" onClick={clicklHandler}>
         Check satus
       </button>
     </section>
