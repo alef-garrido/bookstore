61990fd5976cc8063597b9f6086e4f99b27b3f50
alef-garrido <oa.g11@icloud.com>
2022-03-31 23:47:33 -0600

    Fix extension error on import statement

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,4 +1,4 @@
-import { createStore, applyMiddleware } from '@reduxjs/toolkit';
+import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { logger } from 'redux-logger';
 import rootReducer from './rootReducer';
