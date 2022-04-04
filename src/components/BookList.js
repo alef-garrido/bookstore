38b704b4fb4a4588f201820c3d843834d24e38eb
alef-garrido <oa.g11@icloud.com>
2022-04-04 17:42:50 -0500

    Dispatch loadBooks on BookList rendering

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,19 +1,20 @@
 import { useEffect } from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { loadBooks } from '../redux/books-dux';
 import Book from './Book';
 import BookForm from './BookForm';
 
 function BookList() {
-  const books = useSelector((state) => state.list);
-  useEffect(()  {
-
+  const books = useSelector((state) => state.books.list);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(loadBooks());
   }, [])
   return (
     <div className="book--list--container">
       <section className="book--list">
         <ul>
-          {books.map((book) => <Book key={book.id} book={book} />)}
+        {books ? books.map((book) => <Book id={book[0]} key={book[0]} book={book[1][0]} />) : 'Loading'}
         </ul>
       </section>
       <BookForm />
@@ -21,4 +22,4 @@ function BookList() {
   );
 }
 
-export default Books;
+export default BookList;
