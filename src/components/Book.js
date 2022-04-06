2b387ee0e19aaeb95acf6106c96a9eaf7c21fe41
alef-garrido <oa.g11@icloud.com>
2022-04-06 16:12:47 -0500

    Refactor Book item  styling

--- src/components/Book.js
+++ src/components/Book.js
@@ -15,41 +15,42 @@ function Book(props) {
   const getProgress = (max) => Math.floor(Math.random() * max);
 
   return (
-    <li id={id} className="book--item">
+    <li id={id} className="book--item brdr-gray w-100">
       <div className="book--col-1">
         <div className="book--data">
-          <small className="data--category">{book.category}</small>
-          <p className="data--title">{book.title}</p>
-          <small className="data--author">{book.author}</small>
+          <small className="data--category ff-Mont">{book.category}</small>
+          <p className="data--title w-100 ff-RobSlab">{book.title}</p>
+          <small className="data--author ff-RobSlab fc-blue fw-300">{book.author}</small>
         </div>
         <div className="book--actions">
-          <button type="button">Coments</button>
+          <button className="ff-RobSlab fc-blue brdr-no" type="button">Coments</button>
           <button
+            className="ff-RobSlab fc-blue brdr-no"
             type="button"
             onClick={clickHandler}
           >
             Remove
           </button>
-          <button type="button">Edit</button>
+          <button className="ff-RobSlab fc-blue" type="button">Edit</button>
         </div>
       </div>
-      <div className="book--col-2">
+      <div className="book--col-2 flex-row">
         <img src={loader} alt="" className="loader--display" />
         <div>
-          <p className="progress--digit">
+          <p className="progress--digit ff-Mont">
             {`${getProgress(100)}%`}
           </p>
-          <small className="progress--title">Completed</small>
+          <small className="progress--title ff-Mont fc-gray">Completed</small>
         </div>
       </div>
       <div className="book--col-3">
-        <p className="chapter--title">CURRENT CHAPTER</p>
-        <p className="chapter--info">
+        <p className="chapter--title fc-gray ff-RobSlab fw-300">CURRENT CHAPTER</p>
+        <p className="chapter--info w-100 ff-RobSlab fw-300">
           {' '}
           {`Chapter: ${getProgress(10)}`}
           7
         </p>
-        <button className="chapter--btn" type="button">UPDATE PROGRESS</button>
+        <button className="chapter--bttn fc-white bck-blue ff-RobSlab brdr-no" type="button">UPDATE PROGRESS</button>
       </div>
     </li>
   );
